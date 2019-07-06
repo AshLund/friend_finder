aa32389ee52e60c049ea6f6e9fc56a158cb3f38e
Ash <Ashleigh.lund@gmail.com>
2019-07-06 16:05:50 -0500

    basic site set up

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+  });
+}
+ 
+ 
+
+  
